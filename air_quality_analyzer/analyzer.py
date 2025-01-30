6119669c5ea2646f58c722062c4b4454c4fe6275
amir <amir.es90@yahoo.com>
2025-01-29 17:27:00 -0800

    multithreading works partialy

--- air_quality_analyzer/analyzer.py
+++ air_quality_analyzer/analyzer.py
@@ -1,36 +1,54 @@
 import json
 import requests
-from concurrent.futures import ThreadPoolExecutor
+from threading import Thread, Timer, active_count
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from typing import List, Optional, Tuple, Dict
 
 
-TOKEN = "f75e2f09d2fae1680d7a42a642dfdc7654392b94"
+
 MAP_API = "https://api.waqi.info/v2/map/bounds?latlng={lat1},{lng1},{lat2},{lng2}&networks=all&token={token}"
 GEO_API = "https://api.waqi.info/feed/geo:{lat};{lon}/?token={token}"
 
 class calculate_average_pm25:
     def __init__(self, latitude_1, longitude_1, latitude_2, longitude_2, sampling_period=5, sampling_rate=1):
 
-        self.IDLE       = 0
-        self.RUNNING    = 1
-        self.DONE       = 2
-        self.FAILED     = 3
-        self.STOPPED    = 4
+        self.TOKEN = ""
+
+        self.IDLE       = 'IDLE'
+        self.RUNNING    = 'RUNNING'
+        self.DONE       = 'DONE'
+        self.FAILED     = 'FAILED'
+        self.STOPPED    = 'STOPPED'
         
-        self.state      = self.IDLE
+        self.state      = self.STOPPED
 
         self.latitude_1, self.longitude_1 = latitude_1, longitude_1
         self.latitude_2, self.longitude_2 = latitude_2, longitude_2
+        
 
-        self.sampling_period = sampling_period
-        self.sampling_rate   = sampling_rate
+        self.__sampling_period = sampling_period
+        self.__sampling_rate   = sampling_rate
+
+        self.__stations = [] # holds station coordinates
+        self.pm25data   = []
+
+        self.__worker_threads = []
+        self.thread_cnt       = 8 # can be adjusted for performance
+        
 
 
-    def _worker(self):
-        pass
+    
+    def __handle_api_error(self, error: dict) -> None:
+        """
+        Handle API errors and print the message.
+        
+        Args:
+            error (dict): JSON data containing error message
+        """
+        print(f"Error: {error.get('message')}")
 
 
-    def _extract_stations(self, json_data: dict) -> List[Tuple[float, float]] | None:
+    def __extract_stations(self, json_data: dict) -> List[Tuple[float, float]] | None:
         """
         Extract all the coordinates from Map Querys Json result.
     
@@ -43,8 +61,10 @@ class calculate_average_pm25:
 
         # Check status first
         if json_data.get('status') != 'ok':
+            self._handle_api_error(json_data)
             return None
         
+
         coordinates = []
         for station in json_data.get('data', []):
             lat = station.get('lat')
@@ -57,7 +77,7 @@ class calculate_average_pm25:
         return coordinates
     
     
-    def _extract_pm25(self, json_data: dict) -> float | None:
+    def __extract_pm25(self, json_data: dict) -> float | None:
         """
         Extract PM2.5 value from AQI station data JSON.
         
@@ -80,7 +100,7 @@ class calculate_average_pm25:
             return None
 
 
-    def _get_map_bound(self) -> Dict | None:
+    def __get_map_bound(self) -> Dict | None:
         """
         Get all the stations bounded by input lat and lon using Map Query API.
 
@@ -89,7 +109,7 @@ class calculate_average_pm25:
         """
         url = MAP_API.format(lat1=self.latitude_1, lng1=self.longitude_1, 
                              lat2=self.latitude_2, lng2=self.longitude_2,
-                             token=TOKEN)
+                             token=self.TOKEN)
 
         response = requests.get(url)
         if response.status_code == 200:
@@ -98,7 +118,7 @@ class calculate_average_pm25:
             return None
 
 
-    def _get_station(self, lat: float, lon: float) -> Dict | None:
+    def __get_station(self, lat: float, lon: float) -> Dict | None:
         """
         Get data for a specific station using its lat,lon with Geolocalized API.
 
@@ -109,26 +129,90 @@ class calculate_average_pm25:
         Returns:
             Dict: JSON data containing station and air quality information
         """
-        url = GEO_API.format(lat=lat, lon=lon, token=TOKEN)
+        url = GEO_API.format(lat=lat, lon=lon, token=self.TOKEN)
 
         response = requests.get(url)
         if response.status_code == 200:
             return response.json()
         else:
             return None
-
-
-    def sampling_status(self):
-        return f"Sampling period: {self.sampling_period} seconds, Sampling rate: {self.sampling_rate} Hz"
-
-    def avg_pm25_all_sites(self):
-        return sum([site['pm25'] for site in self.air_quality_data]) / len(self.air_quality_data)
     
+    
+    def __get_pm25(self, station: Tuple[float, float]) -> Optional[float]:
+        """
+        Get PM2.5 value for a specific station.
+
+        Args:
+            station (Tuple[float, float]): Tuple containing lat, lon of the station
+
+        Returns:
+            Optional[float]: PM2.5 value if found, None otherwise
+        """
+        station_data = self.__get_station(*station)
+        pm25_val = self.__extract_pm25(station_data)
+        return pm25_val
+
+    def __worker(self):
+        '''
+        Run multiple threads to get PM2.5 values for all stations.
+        '''
+        results = []
+        with ThreadPoolExecutor(max_workers=self.thread_cnt) as executor:
+            thread_dict = {executor.submit(self.__get_pm25, st): st for st in self.__stations}
+
+            for thread in as_completed(thread_dict):
+                station = thread_dict[thread]
+
+                try:
+                    pm25_val = thread.result()
+                    if pm25_val is not None:
+                        results.append(pm25_val)
+
+                except Exception as exc:
+                    print(f'{station} generated an exception: {exc}')
+    
+    def _worker_thread_wrapper(self):
+        '''
+        Wrapper function to run the worker function in a thread.
+        '''
+        work_thread = Thread(target=self.__worker)
+        work_thread.start()
+
+
     def start_sampling(self):
-        pass
+        if self.TOKEN: # check if token is set
+            if self.__stations == []: # if stations are not already extracted
+                self.__stations = self.__extract_stations(self.__get_map_bound())
+        else:
+            print("Error: Token not set. use set_token()")
+            self.state = self.FAILED
+            return
+        
+        self.state = self.RUNNING
+
+        for delay in range(0, self.__sampling_period * 60, 60 // self.__sampling_rate):
+            
+            # Non-blocking timers to run worker threads at sampling rate
+            self.__worker_threads.append(Timer(delay , self._worker_thread_wrapper))
+            self.__worker_threads[-1].start()
+        
     
     def stop_sampling(self):
         pass
+
+
+    def sampling_status(self):
+        if active_count() == 1:
+            self.state = self.DONE
+        return f"worker threads: {len(self.__worker_threads)} | state: {self.state}"
+
+
+    def avg_pm25_all_sites(self):
+        pass
+    
+    def set_token(self, token):
+        self.TOKEN = token
+
     
 
         
--- sample_code.py
+++ sample_code.py
@@ -1,6 +1,19 @@
 from air_quality_analyzer.analyzer import calculate_average_pm25
+import time
 
-a = calculate_average_pm25(48.441591, -123.377021, 49.201088, -122.7613762)
-b = a._get_station(49.1414, -123.1083)
-c = a._extract_pm25(b)
-print(c)
+my_obj = calculate_average_pm25(48.441591, -123.377021, 49.201088, -122.7613762)
+
+my_obj.set_token("f75e2f09d2fae1680d7a42a642dfdc7654392b94")
+
+my_obj.start_sampling()
+
+run = True
+while run:
+    input_val = input("Enter 'stop' to stop sampling: ")
+    if input_val == 'stop':
+        my_obj.stop_sampling()
+        run = False
+    else:
+        print(my_obj.sampling_status())
+
+print(my_obj.sampling_status())
